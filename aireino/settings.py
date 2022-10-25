cd7b2d445c1da92bf0a6af2602a010def879f268
LarsenMathias <larsenm2020@gmail.com>
2022-10-25 22:29:45 +0530

    sd

--- aireino/settings.py
+++ aireino/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/","static/images"),
+    os.path.join(BASE_DIR, "static/"),
 )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT=os.path.normpath(os.path.join(BASE_DIR,'static'))
+STATIC_ROOT=os.path.normpath(os.path.join(BASE_DIR,'staticfiles'))
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
