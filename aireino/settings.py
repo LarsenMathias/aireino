c440c452d7ede586476733be378db26d779cf537
LarsenMathias <larsenm2020@gmail.com>
2022-10-25 22:01:25 +0530

    u[date setting

--- aireino/settings.py
+++ aireino/settings.py
@@ -129,3 +129,5 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT=os.path.normpath(os.path.join(BASE_DIR,'staticfiles'))
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
