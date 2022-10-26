893345a9e2d653b4fb186f430d71ac240f2cac0a
LarsenMathias <larsenm2020@gmail.com>
2022-10-26 11:29:45 +0530

    update settings

--- aireino/urls.py
+++ aireino/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from unicodedata import name
 from django.contrib import admin
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns,urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
@@ -38,8 +38,8 @@ urlpatterns = [
     path('',index,name="wrightflight.png"),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
- static(settings.STATIC_URL,document_root=settings.STATIC_ROOT),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+ urlpatterns.extend(static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)),
+   urlpatterns.extend( static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)),
 
 ]
 
